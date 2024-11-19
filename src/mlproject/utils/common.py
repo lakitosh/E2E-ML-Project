a16c2bc4844b982a787a75ff7dcf4b2f519d9ffd
Lakitosh Singh <lakitosh@gmail.com>
2024-11-19 05:49:24 -0600

    testing

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -12,7 +12,8 @@ from typing import Any
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
-        with open(path_to_yaml) as yaml_file:
+        print(f"Attempting to read YAML file from: {os.path.abspath(path_to_yaml)}")
+        with open(path_to_yaml, "r") as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
